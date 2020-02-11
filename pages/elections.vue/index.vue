ed47aa00accf04669244b3c4b63b7ada1fba677f
Enrique <enriquep36@hotmail.com>
2020-02-10 19:54:07 -0600

    limpieza template

---
+++ pages/elections.vue/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+      <blockquote class="blockquote">
+        Hola
+
+          <v-btn
+            color="primary"
+            nuxt
+            to="/"
+          >
+            Continue
+          </v-btn>
+      </blockquote>
+    </v-flex>
+  </v-layout>
+</template>
